f849dd0a82001692b3bfe07fb604c25e575118b9
mateuszokroj1 <mateuszokroj1@gmail.com>
2020-11-10 21:46:05 +0100

    Dodano enum TaskState. Dodano klasy Task, TaskDefaultNameGenerator, TaskEventHandler, TaskErrorEventHandler, TaskEventArgs, TaskErrorEventArgs. Dodano interfejsy ITask, IGenerator, ITaskErrorEventArgs, ITaskEventArgs.

---
+++ Sources/Core/Enums/TaskState.cs
@@ -0,0 +1,43 @@
+﻿namespace FileManipulator
+{
+    /// <summary>
+    /// Representing actual task working state
+    /// </summary>
+    public enum TaskState
+    {
+        /// <summary>
+        /// On undefined state
+        /// </summary>
+        Undefined,
+
+        /// <summary>
+        /// When exception is handled
+        /// </summary>
+        Error,
+
+        /// <summary>
+        /// Before start
+        /// </summary>
+        Ready,
+
+        /// <summary>
+        /// When stopped
+        /// </summary>
+        Stopped,
+
+        /// <summary>
+        /// During work
+        /// </summary>
+        Working,
+        
+        /// <summary>
+        /// When task is paused
+        /// </summary>
+        Paused,
+
+        /// <summary>
+        /// On work completed
+        /// </summary>
+        Done
+    }
+}
---
+++ Sources/Core/Models/ITask.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace FileManipulator
+{
+    public interface ITask : IDisposable, INotifyPropertyChanged
+    {
+        #region Properties
+
+        TaskState State { get; }
+
+        IProgress<float> Progress { get; }
+
+        string Name { get; set; }
+
+        Guid Id { get; }
+
+        #endregion
+
+        #region Methods
+
+        Task StartAsync();
+
+        Task StopAsync();
+
+        Task PauseAsync();
+
+        #endregion
+    }
+}
