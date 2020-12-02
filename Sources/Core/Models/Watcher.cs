7b83052b0e95975224af91bff14ead6a6694b86a
mateuszokroj1 <mateuszokroj1@gmail.com>
2020-12-02 20:40:00 +0100

    Created Watcher class.

---
+++ Sources/Core/Models/Watcher.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+using STT = System.Threading.Tasks;
+
+namespace FileManipulator
+{
+    public class Watcher : Task
+    {
+        #region Constructor
+
+        public Watcher(IEnumerable<Task> tasks)
+        {
+            var generator = new TaskDefaultNameGenerator<Watcher>(tasks);
+            Name = generator.Generate();
+            ResetAsync().Wait();
+            State = TaskState.Ready;
+        }
+
+        #endregion
+
+        #region Fields
+
+        private string path;
+
+        #endregion
+
+        #region Properties
+
+        public string Path
+        {
+            get => this.path;
+            set => SetProperties(ref this.path, value, nameof(Path), nameof());
+        }
+
+        public bool PathIsValid 
+
+        #endregion
+
+        #region Methods
+
+        public override STT.Task PauseAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override STT.Task ResetAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override STT.Task StartAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override STT.Task StopAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
