01cb86378bf53aa3ec10a70a473de5cb0cebd24c
mateuszokroj1 <mateuszokroj1@gmail.com>
2020-12-03 16:31:47 +0100

    Added observable events to TaskProgress class.

---
+++ Sources/Core/Models/TaskProgress.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reactive.Linq;
+
+namespace FileManipulator
+{
+    public class TaskProgress : ModelBase, IProgress<float>
+    {
+        #region Constructor
+
+        public TaskProgress()
+        {
+            OnValueChanged =
+                PropertyChangedObservable
+                .Where(propertyName => propertyName == nameof(ProgressValue))
+                .Select(p => ProgressValue);
+
+            OnStatusChanged =
+                PropertyChangedObservable
+                .Where(propertyName => propertyName == nameof(Status))
+                .Select(p => Status);
+        }
+
+        #endregion
+
+        #region Fields
+
+        private float progressValue;
+        private string status;
+
+        #endregion
+
+        #region Properties
+
+        public string Status
+        {
+            get => this.status;
+            set => SetProperty(ref this.status, value);
+        }
+
+        public float ProgressValue
+        {
+            get => this.progressValue;
+            set => SetProperty(ref this.progressValue, value);
+        }
+
+        public IObservable<float> OnValueChanged { get; }
+
+        public IObservable<string> OnStatusChanged { get; }
+
+        #endregion
+
+        #region Methods
+
+        public void Report(float value)
+        {
+            ProgressValue = value;
+        }
+
+        public void Report(float value, string status)
+        {
+            Report(value);
+            Status = status;
+        }
+
+        #endregion
+    }
+}
