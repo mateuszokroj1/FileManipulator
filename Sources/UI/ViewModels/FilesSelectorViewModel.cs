80f6f849565e86bf38d3b4f209915ef9084a5bee
mateuszokroj1 <mateuszokroj1@gmail.com>
2020-12-04 23:51:06 +0100

    Added AlphanumericComparer class. Added BoolToVisibilityConverter. Created FilesSelectorControl. Created TasksView control.

---
+++ Sources/UI/ViewModels/FilesSelectorViewModel.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Windows.Input;
+
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace FileManipulator.UI
+{
+    public class FilesSelectorViewModel : ModelBase
+    {
+        #region Constructor
+
+        public FilesSelectorViewModel()
+        {
+            BrowseCommand = new Command(() => Browse());
+            DeleteCommand = new ReactiveCommand(
+               PropertyChangedObservable
+               .Where(name => name == nameof(SelectedFile))
+               .Select(n => Files.Contains(SelectedFile)),
+               () => DeleteSelectedFile()
+            );
+            ClearCommand = new Command(() => ClearList());
+        }
+
+        #endregion
+
+        #region Fields
+
+        private SelectionType selectionType = SelectionType.Directory;
+        private string directory;
+        private bool isDirectoryValid;
+        private bool isFilesValid;
+        private string selectedFile;
+
+        #endregion
+
+        #region Properties
+
+        public SelectionType SelectionType
+        {
+            get => this.selectionType;
+            set => SetProperty(ref this.selectionType, value, nameof(SelectionType), nameof(SelectedFiles));
+        }
+
+        public ObservableCollection<string> Files { get; } = new ObservableCollection<string>();
+
+        public string SelectedFile
+        {
+            get => this.selectedFile;
+            set => SetProperty(ref this.selectedFile, value);
+        }
+
+        public string Directory
+        {
+            get => this.directory;
+            set => SetProperty(ref this.directory, value, nameof(Directory), nameof(SelectedFiles));
+        }
+
+        public bool IsDirectoryValid
+        {
+            get => this.isDirectoryValid;
+            private set => SetProperty(ref this.isDirectoryValid, value);
+        }
+
+        public bool IsFilesValid
+        {
+            get => this.isFilesValid;
+            private set => SetProperty(ref this.isFilesValid, value);
+        }
+
+        public IEnumerable<string> SelectedFiles => Generate();
+
+        public ICommand BrowseCommand { get; }
+
+        public ICommand DeleteCommand { get; }
+
+        public ICommand ClearCommand { get; }
+
+        #endregion
+
+        #region Methods
+
+        public IEnumerable<string> Generate()
+        {
+            switch(SelectionType)
+            {
+                case SelectionType.Directory:
+                    if(!string.IsNullOrEmpty(Directory) && System.IO.Directory.Exists(Directory))
+                    {
+                        IEnumerable<string> files;
+
+                        try
+                        {
+                            files = System.IO.Directory.EnumerateFiles(Directory, "", SearchOption.TopDirectoryOnly);
+                            IsDirectoryValid = true;
+                            return files;
+                        }
+                        catch(Exception)
+                        {
+                            IsDirectoryValid = false;
+                            return Enumerable.Empty<string>();
+                        }
+                    }
+                    else
+                    {
+                        IsDirectoryValid = false;
+                        return Enumerable.Empty<string>();
+                    }
+                case SelectionType.Files:
+                    foreach (var path in Files)
+                    {
+                        try
+                        {
+                            if (!File.Exists(path))
+                            {
+                                IsFilesValid = false;
+                                return Enumerable.Empty<string>();
+                            }
+                        }
+                        catch (Exception)
+                        {
+                            IsFilesValid = false;
+                            return Enumerable.Empty<string>();
+                        }
+                    }
+
+                    IsFilesValid = true;
+                    return Files;
+                default:
+                    return Enumerable.Empty<string>();
+            }
+        }
+
+        public void Browse()
+        {
+            if(SelectionType == SelectionType.Directory)
+            {
+                using (var dialog = new CommonOpenFileDialog())
+                {
+                    dialog.AllowNonFileSystemItems = false;
+                    dialog.Title = "Wybierz katalog";
+                    dialog.IsFolderPicker = true;
+                    dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+                    dialog.EnsurePathExists = true;
+                    dialog.Multiselect = false;
+                    dialog.NavigateToShortcut = true;
+                    dialog.ShowHiddenItems = false;
+
+                    if (dialog.ShowDialog(App.Current.MainWindow) != CommonFileDialogResult.Ok) return;
+
+                    Directory = dialog.FileName;
+                }
+            }
+            else if(SelectionType == SelectionType.Files)
+            {
+                using (var dialog = new CommonOpenFileDialog())
+                {
+                    dialog.AllowNonFileSystemItems = false;
+                    dialog.Title = "Wybierz pliki do dodania";
+                    dialog.IsFolderPicker = false;
+                    dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+                    dialog.EnsureFileExists = true;
+                    dialog.Multiselect = true;
+                    dialog.NavigateToShortcut = true;
+                    dialog.ShowHiddenItems = false;
+                    dialog.EnsureReadOnly = false;
+
+                    if (dialog.ShowDialog(App.Current.MainWindow) != CommonFileDialogResult.Ok) return;
+
+                    if (dialog.FileNames.Count() < 1) return;
+
+                    try
+                    {
+                        if (dialog.FileNames.Where(file => !File.Exists(file)).Count() > 0) return;
+                    }
+                    catch(Exception)
+                    {
+                        return;
+                    }
+
+                    foreach (var file in dialog.FileNames)
+                        Files.Add(file);
+                }
+            }
+        }
+
+        public void DeleteSelectedFile()
+        {
+            if (Files.Contains(SelectedFile))
+                Files.Remove(SelectedFile);
+        }
+
+        public void ClearList() => Files.Clear();
+
+        #endregion
+    }
+}
---
+++ Sources/UI/Views/Controls/FilesSelectorControl.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace FileManipulator.UI
+{
+    /// <summary>
+    /// Logika interakcji dla klasy FilesSelectorControl.xaml
+    /// </summary>
+    public partial class FilesSelectorControl : UserControl
+    {
+        public FilesSelectorControl()
+        {
+            InitializeComponent();
+            var model = DataContext as FilesSelectorViewModel;
+            var binding = new Binding("SelectedFiles");
+            binding.Source = model;
+            binding.Mode = BindingMode.TwoWay;
+            SetBinding(SelectedFilesProperty, binding);
+        }
+
+        public static readonly DependencyProperty SelectedFilesProperty = DependencyProperty.Register(
+            "SelectedFiles",
+            typeof(IEnumerable<string>),
+            typeof(FilesSelectorControl),
+            new PropertyMetadata(null)
+        );
+
+        public IEnumerable<string> SelectedFiles => GetValue(SelectedFilesProperty) as IEnumerable<string>;
+    }
+}
