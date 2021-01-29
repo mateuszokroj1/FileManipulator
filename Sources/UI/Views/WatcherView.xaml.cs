c10e94e9bd2b66f08049628465fd82725f56e3f5
mateuszokroj1 <mateuszokroj1@gmail.com>
2021-01-29 15:01:30 +0100

    Bug#255 - Dodano przypisanie ścieżki z Viewmodel do modelu.

--- Sources/UI/Views/WatcherView.xaml.cs
+++ Sources/UI/Views/WatcherView.xaml.cs
@@ -1,9 +1,10 @@
-﻿using System.Windows;
+﻿using System.Text;
+using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 
-using FileManipulator.Models.Watcher;
 using FileManipulator.ViewModels;
+
+using Microsoft.WindowsAPICodePack.Dialogs;
 
 namespace FileManipulator.UI
 {
@@ -19,8 +20,37 @@ namespace FileManipulator.UI
 
         private void Root_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
         {
-            if(DataContext is WatcherViewModel model)
+            if (DataContext is WatcherViewModel model)
+            {
                 model.OnInvalidInput = () => MessageBox.Show("Podano niepoprawną ścieżkę.", "Watcher", MessageBoxButton.OK, MessageBoxImage.Warning);
+
+                model.OnError = exc =>
+                {
+                    using (var dialog = new TaskDialog())
+                    {
+                        dialog.Cancelable = false;
+                        dialog.Caption = "Watcher";
+                        dialog.Text = "Wystąpił błąd podczas wykonywania zadania dlatego zostało przerwane.";
+                        dialog.Icon = TaskDialogStandardIcon.Error;
+                        dialog.DetailsExpanded = false;
+                        dialog.DetailsCollapsedLabel = dialog.DetailsExpandedLabel = "Więcej informacji";
+                        dialog.ExpansionMode = TaskDialogExpandedDetailsLocation.ExpandContent;
+                        dialog.StartupLocation = TaskDialogStartupLocation.CenterScreen;
+                        dialog.StandardButtons = TaskDialogStandardButtons.Ok;
+
+                        var builder = new StringBuilder();
+
+                        builder.AppendLine(exc.GetType().FullName);
+                        builder.AppendLine($"Message: {exc.Message}");
+                        builder.AppendLine($"Stacktrace:");
+                        builder.Append(exc.StackTrace);
+
+                        dialog.DetailsExpandedText = builder.ToString();
+
+                        dialog.Show();
+                    }
+                };
+            }
         }
     }
 }
