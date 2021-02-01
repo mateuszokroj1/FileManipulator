0da548cf2ac306f435c1aa632e01f2c145fc32db
mateuszokroj1 <mateuszokroj1@gmail.com>
2021-02-01 19:06:23 +0100

    Naprawiono komendy w menu głównym.

--- Sources/UI/Views/Windows/RenameWindow.xaml.cs
+++ Sources/UI/Views/Windows/RenameWindow.xaml.cs
@@ -1,11 +1,12 @@
 ﻿using System.Windows;
+using System.Windows.Input;
 
 namespace FileManipulator.UI
 {
     /// <summary>
     /// Logika interakcji dla klasy RenameWindow.xaml
     /// </summary>
-    public partial class RenameWindow : Window, ITextDialog
+    public partial class RenameWindow : Window
     {
         public RenameWindow()
         {
@@ -22,13 +23,40 @@ namespace FileManipulator.UI
 
         private void TextBox_LostFocus(object sender, RoutedEventArgs e)
         {
-            if(string.IsNullOrWhiteSpace(Value))
+            if(string.IsNullOrWhiteSpace(Text.Text))
             {
                 MessageBox.Show("Nazwa musi zawierać jakiś znak widzialny.", "Błędna wartość", MessageBoxButton.OK, MessageBoxImage.Warning);
                 this.Text.Focus();
             }
         }
 
-        bool ITextDialog.ShowDialog() => base.ShowDialog() ?? false;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(Text.Text))
+            {
+                MessageBox.Show("Nazwa musi zawierać jakiś znak widzialny.", "Błędna wartość", MessageBoxButton.OK, MessageBoxImage.Warning);
+                this.Text.Focus();
+                return;
+            }
+
+            Value = this.Text.Text;
+            DialogResult = true;
+        }
+
+        private void Window_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                DialogResult = false;
+                Close();
+            }
+            else if (e.Key == Key.Enter)
+                Button_Click(this, e);
+        }
+
+        private void Window_Activated(object sender, System.EventArgs e)
+        {
+            Text.Text = Value;
+        }
     }
 }
